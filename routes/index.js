c74acf71bb8f0fb1b15e8dc3abe2b6587e790fac
Tom <tvalenti@valentionline.com>
2020-02-07 10:02:25 -0500

    added dynamic version number in title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node.js Express App in Docker' });
+  res.render('index', { title: `Node.js Express App in Docker ${process.env.version}` });
 });
 
 module.exports = router;

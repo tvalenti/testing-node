fabb4ec6074d5dedcf36d1673ca5c0675c303b73
Tom <tvalenti@valentionline.com>
2020-02-07 15:52:56 -0500

    version change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: `Node.js Express App in Docker ${process.env.version}` });
+  res.render('index', { title: `Node.js Express App in Docker ${process.env.npm_package_version}` });
 });
 
 module.exports = router;
